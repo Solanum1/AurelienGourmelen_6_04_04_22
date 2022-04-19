7786a4dbdbc398385698c492e46e430df907c9c6
Aurélien Gourmelen <aurelien.gourmelen@gmail.com>
2022-04-19 17:15:13 +0200

    Ajout des middlewares pour controle email et password

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,15 @@
 const express = require("express");
 const router = express.Router();
 
+const email = require("../middleware/email");
+
+const password = require("../middleware/password");
+
 const userCtrl = require("../controllers/userCtrl");
 
-//--------------------------Router-------------------------------
+//--------------------------Routers-------------------------------
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", email, password, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 //--------------------------Exportation--------------------------
